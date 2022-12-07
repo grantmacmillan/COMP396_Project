f97d16d944fa08f552992e88c8008122a4640f23
Ben Coombes <benzcoombes@gmail.com>
2022-12-07 12:04:34 -0500

    menu sounds/music

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cursor = UnityEngine.Cursor;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
