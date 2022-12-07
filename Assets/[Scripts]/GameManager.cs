a45d31aac1698834ea9b4b6593d88cf036968529
Ben Coombes <benzcoombes@gmail.com>
2022-12-07 15:45:39 -0500

    Pause menu and sensitivity setting

--- Assets/[Scripts]/GameManager.cs
+++ Assets/[Scripts]/GameManager.cs
@@ -6,9 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
     public bool briefcaseCollected = false;
     public GameObject winZone;
-    
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,5 +40,21 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    
+    public void Pause(GameObject obj)
+    {
+        obj.SetActive(true);
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        InputManager.Instance.SwitchActionMap();
+        Time.timeScale = 0f;
+    }
+
+    public void Resume(GameObject obj)
+    {
+        obj.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        InputManager.Instance.SwitchActionMap();
+        Time.timeScale = 1f;
+    }
 }
